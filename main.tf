f4dfcb7edc77d044d296e516ffbf8b360dffcc0d
Pradeep <114085306+PradeepBollepalli@users.noreply.github.com>
2023-05-17 23:33:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws"{
     region = "us-east-2"
-    access_key ="AKIASLY6UW5JB65UU3YH"
-    secret_key ="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"
+    access_key =""
+    secret_key =""
 }
 resource "aws_vpc" "first_VPC" {
     cidr_block = "10.0.0.0/16"
